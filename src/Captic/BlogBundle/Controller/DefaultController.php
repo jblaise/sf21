256fd1ea2e6945c0cd960fa6a067f04c344b261f
Ju <no@in.pub>
2016-06-09 18:55:31 +0200

    add welcome

--- src/Captic/BlogBundle/Controller/DefaultController.php
+++ src/Captic/BlogBundle/Controller/DefaultController.php
@@ -9,10 +9,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/", name="welcome")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/hello/{name}", name="greetings")
+     * @Template()
+     */
+    public function helloAction($name)
     {
         return array('name' => $name);
     }
