963e90045e98d50147449648cb7579d5e178580e
Ju <no@in.pub>
2016-06-10 11:28:12 +0200

    add blog

---
+++ src/Captic/BlogBundle/Controller/PostController.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Captic\BlogBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Captic\BlogBundle\Entity\Post;
+use Captic\BlogBundle\Form\PostType;
+
+/**
+ * Post controller.
+ *
+ * @Route("/post_admin")
+ */
+class PostController extends Controller
+{
+    /**
+     * Lists all Post entities.
+     *
+     * @Route("/", name="post_admin")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('CapticBlogBundle:Post')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Finds and displays a Post entity.
+     *
+     * @Route("/{id}/show", name="post_admin_show")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('CapticBlogBundle:Post')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Post entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Post entity.
+     *
+     * @Route("/new", name="post_admin_new")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Post();
+        $form   = $this->createForm(new PostType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a new Post entity.
+     *
+     * @Route("/create", name="post_admin_create")
+     * @Method("POST")
+     * @Template("CapticBlogBundle:Post:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Post();
+        $form = $this->createForm(new PostType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('post_admin_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Post entity.
+     *
+     * @Route("/{id}/edit", name="post_admin_edit")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('CapticBlogBundle:Post')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Post entity.');
+        }
+
+        $editForm = $this->createForm(new PostType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Post entity.
+     *
+     * @Route("/{id}/update", name="post_admin_update")
+     * @Method("POST")
+     * @Template("CapticBlogBundle:Post:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('CapticBlogBundle:Post')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Post entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new PostType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('post_admin_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Post entity.
+     *
+     * @Route("/{id}/delete", name="post_admin_delete")
+     * @Method("POST")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('CapticBlogBundle:Post')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Post entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('post_admin'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Captic/BlogBundle/Form/PostType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Captic\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('content')
+            ->add('date')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Captic\BlogBundle\Entity\Post'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'captic_blogbundle_posttype';
+    }
+}
